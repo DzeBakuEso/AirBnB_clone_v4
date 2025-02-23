cbeaae2c3eb9ccba1ed949ef333cd76226fc75d4
DzeBakuEso <dzeblebaku@gmail.com>
2025-02-23 08:13:24 -1200

    1-hbnb.py, templates/1-hbnb.html, static/scripts/1-hbnb.js

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -10,12 +10,12 @@ import uuid  # Import UUID for cache busting
 
 app = Flask(__name__)
 
-@app.teardown_appcontext
+@app.teardown_appcontext   
 def close_db(error):
     """ Remove the current SQLAlchemy Session """
     storage.close()
 
-@app.route('/0-hbnb/', strict_slashes=False)
+@app.route('/1-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -35,7 +35,7 @@ def hbnb():
     cache_id = uuid.uuid4()
 
     return render_template(
-        '0-hbnb.html',  # Changed to use 0-hbnb.html
+        '1-hbnb.html',  # Changed to use 1-hbnb.html
         states=st_ct,
         amenities=amenities,
         places=places,
---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    let selectedAmenities = {};
+
+    // Listen for changes on each input checkbox tag
+    $('.amenities input[type="checkbox"]').change(function () {
+        const amenityId = $(this).attr('data-id');
+        const amenityName = $(this).attr('data-name');
+
+        if ($(this).is(':checked')) {
+            selectedAmenities[amenityId] = amenityName;
+        } else {
+            delete selectedAmenities[amenityId];
+        }
+
+        // Update the h4 tag inside the div Amenities with the list of selected amenities
+        $('.amenities h4').text(Object.values(selectedAmenities).join(', ') || '\u00A0');
+    });
+});
